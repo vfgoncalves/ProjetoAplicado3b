a4cf0c9f868c8030e373153cfae9f967649e2bf9
Victor Ferreira <vgoncalves021993@hotmail.com>
2017-04-05 19:11:36 -0300

    Inicializando desenvolvimento para mostrar lista de vagas

--- app/src/main/java/projetoaplicado/vgoncalves/com/projetoaplicado/VagasEmpresaActivity.java
+++ app/src/main/java/projetoaplicado/vgoncalves/com/projetoaplicado/VagasEmpresaActivity.java
@@ -5,10 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import projetoaplicado.vgoncalves.com.projetoaplicado.Config.ConfiguracaoFirebase;
+import projetoaplicado.vgoncalves.com.projetoaplicado.Model.Vaga;
 
 public class VagasEmpresaActivity extends AppCompatActivity {
 
     private FloatingActionButton btnCadastrarVaga;
+    private ListView listVagas;
+    private ArrayAdapter adapter;
+    private ArrayList<String> tituloVaga;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +32,34 @@ public class VagasEmpresaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_vagas_empresa);
 
         btnCadastrarVaga = (FloatingActionButton) findViewById(R.id.btnCadastrarVaga);
+        databaseReference = ConfiguracaoFirebase.getReferenceFirebase().child(ConfiguracaoFirebase.NODE_VAGA);
+
+        //Iniciar listview para mostrar vagas
+        listVagas = (ListView) findViewById(R.id.listaVagas);
+        tituloVaga = new ArrayList<>();
+        adapter = new ArrayAdapter(
+                getApplicationContext(),
+                R.layout.lista_vagas,
+                tituloVaga
+        );
+        listVagas.setAdapter(adapter);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                /*for(DataSnapshot dados: dataSnapshot.getChildren()){
+                    Vaga vaga = dados.getValue(Vaga.class);
+                    tituloVaga.add(vaga.getTitulo());
+                }
+                adapter.notifyDataSetChanged();*/
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         btnCadastrarVaga.setOnClickListener(new View.OnClickListener() {
             @Override
