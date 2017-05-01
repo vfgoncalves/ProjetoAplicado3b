204cd2b3986388d75def2f2c0d5af2018f428db9
Victor Ferreira <vgoncalves021993@hotmail.com>
2017-05-01 12:16:37 -0300

    Implementação de cadastro via linkedin (cadastro para Candidatos)

--- app/src/main/java/projetoaplicado/vgoncalves/com/projetoaplicado/view/activity/CadastrarActivity.java
+++ app/src/main/java/projetoaplicado/vgoncalves/com/projetoaplicado/view/activity/CadastrarActivity.java
@@ -28,7 +28,18 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.linkedin.platform.APIHelper;
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIApiError;
+import com.linkedin.platform.errors.LIAuthError;
+import com.linkedin.platform.listeners.ApiListener;
+import com.linkedin.platform.listeners.ApiResponse;
+import com.linkedin.platform.listeners.AuthListener;
+import com.linkedin.platform.utils.Scope;
 
+import org.json.JSONObject;
+
+import projetoaplicado.vgoncalves.com.projetoaplicado.Model.Empresa;
 import projetoaplicado.vgoncalves.com.projetoaplicado.Model.Usuario;
 import projetoaplicado.vgoncalves.com.projetoaplicado.R;
 import projetoaplicado.vgoncalves.com.projetoaplicado.controller.Controller;
@@ -39,11 +50,16 @@ public class CadastrarActivity extends AppCompatActivity {
     private EditText senha;
     private Button btnCadastrar;
     private ImageButton imgSignGoogle;
+    private ImageButton imgSignLinkedin;
     private GoogleApiClient mGoogleApiClient;
     private Usuario usuario;
     private FirebaseAuth autenticacao;
     private Controller controller;
     private ProgressDialog progressDialog;
+    private ProgressDialog progressDialogLinkedin;
+
+    private boolean authGoogle = false;
+    private boolean authLinkedin = false;
 
     //Constantes
     private static final int RC_SIGN_IN = 9001;
@@ -58,12 +74,16 @@ public class CadastrarActivity extends AppCompatActivity {
         senha = (EditText) findViewById(R.id.editCadSenha);
         btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
         imgSignGoogle = (ImageButton) findViewById(R.id.imgSignGoogle);
+        imgSignLinkedin = (ImageButton) findViewById(R.id.imgSignLinkedin);
 
         //Configurando progressDialog
         progressDialog = new ProgressDialog(CadastrarActivity.this);
-        progressDialog.setTitle("Cadastrando");
         progressDialog.setMessage("Aguarde, estamos efetuando o cadastro");
         progressDialog.setCancelable(false);
+
+        progressDialogLinkedin = new ProgressDialog(CadastrarActivity.this);
+        progressDialogLinkedin.setMessage("Aguarde, estamos efetuando o cadastro com a conta Linkedin");
+        progressDialogLinkedin.setCancelable(false);
 
         controller = new Controller(CadastrarActivity.this);
         autenticacao = controller.getAutenticador();
@@ -94,7 +114,18 @@ public class CadastrarActivity extends AppCompatActivity {
         imgSignGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                authGoogle = true;
+                authLinkedin = false;
                 signIn();
+            }
+        });
+
+        imgSignLinkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authGoogle = false;
+                authLinkedin = true;
+                signInLinkedin();
             }
         });
     }
@@ -159,10 +190,19 @@ public class CadastrarActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            progressDialog.show();
-            handleSignInResult(result);
+
+        if(authLinkedin){
+            //AUTENTICAÇÃO COM LINKEDIN
+
+            // Add this line to your existing onActivityResult() method
+            progressDialogLinkedin.show();
+            LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
+        }else if(authGoogle){
+            if (requestCode == RC_SIGN_IN) {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                progressDialog.show();
+                handleSignInResult(result);
+            }
         }
     }
     private void handleSignInResult(GoogleSignInResult result) {
@@ -209,4 +249,92 @@ public class CadastrarActivity extends AppCompatActivity {
             mostraMensagem("Erro ao efetuar login com conta Google: " + e.getMessage());
         }
     }
+    private void signInLinkedin(){
+        LISessionManager.getInstance(getApplicationContext()).init(this, buildScope(), new AuthListener() {
+            @Override
+            public void onAuthSuccess() {
+                buscarInfoPessoal();
+            }
+
+            @Override
+            public void onAuthError(LIAuthError error) {
+                mostraMensagem("Erro ao logar com linkedin");
+                progressDialogLinkedin.hide();
+                Log.d("AuthLinkedin", error.toString());
+            }
+        }, true);
+    }
+    private static Scope buildScope() {
+        return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE, Scope.R_EMAILADDRESS);
+    }
+    private void buscarInfoPessoal(){
+        String url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,public-profile-url,picture-url,email-address)";
+
+        APIHelper apiHelper = APIHelper.getInstance(getApplicationContext());
+        apiHelper.getRequest(this, url, new ApiListener() {
+            @Override
+            public void onApiSuccess(ApiResponse apiResponse) {
+                // Success!
+                try{
+                    cadastrarLinkedin(apiResponse.getResponseDataAsJson());
+                }catch (Exception e){
+                    e.getMessage();
+                }
+            }
+
+            @Override
+            public void onApiError(LIApiError liApiError) {
+                // Error making GET request!
+            }
+        });
+    }
+    private void cadastrarLinkedin(final JSONObject user){
+        try{
+            String senha = controller.geraSenhaLinkedin(user.getString("emailAddress"));
+            autenticacao.createUserWithEmailAndPassword(user.getString("emailAddress"),senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    try{
+                        if (task.isSuccessful()){
+                            mostraMensagem("Usuário cadastrada com sucesso");
+                            //salvar dados do usuário
+                            usuario = new Usuario(CadastrarActivity.this);
+                            usuario.setID(task.getResult().getUser().getUid());
+                            usuario.setNome(user.getString("firstName") + " " + user.getString("lastName"));
+                            usuario.setEmail(user.getString("emailAddress"));
+                            usuario.setPhotoUrl(user.getString("pictureUrl"));
+                            usuario.salvar();
+
+                            //Navegar até tela main - Tela principal
+                            Intent intent = new Intent(CadastrarActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                            progressDialogLinkedin.hide();
+                        }else{
+                            try{
+                                progressDialog.hide();
+                                throw task.getException();
+                            }catch (FirebaseAuthWeakPasswordException e){
+                                mostraMensagem("Digite uma senha mais forte e longa!");
+                            }catch (FirebaseAuthInvalidCredentialsException e){
+                                mostraMensagem("E-mail inválido, por favor digitar um novo e-mail!");
+                            }catch (FirebaseAuthUserCollisionException e){
+                                mostraMensagem("E-mail já está em uso, por favor digite outro e-mail");
+                            }catch (Exception e){
+                                mostraMensagem("Erro ao cadastrar usuário, tente novamente!");
+                            }
+                        }
+                    }catch (Exception e){
+                        e.getMessage();
+                    }
+                }
+            });
+        }catch (Exception e){
+            mostraMensagem("Erro ao logar com a conta Linkedin");
+            progressDialogLinkedin.hide();
+            e.getMessage();
+        }
+    }
+
 }
